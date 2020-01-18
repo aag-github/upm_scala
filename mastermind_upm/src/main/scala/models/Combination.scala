12a83090bbbf9a7b64f7a2b74fb0050c16b5e7b5
aag-github <aag_uk@yahoo.com>
2020-01-18 13:19:31 +0100

    Revert "cambiadas reglas a 4 colores y 15 intentos"
    This reverts commit c5d36d5053f9710d0045eed912aa35efd7241930.

--- mastermind_upm/src/main/scala/models/Combination.scala
+++ mastermind_upm/src/main/scala/models/Combination.scala
@@ -4,7 +4,7 @@ object Combination {
   val COLORS_STRING = "RGBYOW"
   val COLORS_LIST = COLORS_STRING.toList
   val MAX_COLORS = COLORS_LIST.length
-  val LENGTH = 4
+  val LENGTH = 6
 }
 
 class Combination(list : List[Char]) {
